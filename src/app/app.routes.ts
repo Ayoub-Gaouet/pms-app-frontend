ca08badf4ce528238d419467795b5fae3a946aed
Ayoub Gaouet <gaouetayoub1@gmail.com>
2026-03-01 16:41:38 +0100

    update navbar styling and routing; add product creation component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
-import {ProductList} from './features/product/pages/product-list/product-list';
+import { ProductList } from './features/product/pages/product-list/product-list';
+import { ProductCreate } from './features/product/pages/product-create/product-create';
 
 export const routes: Routes = [
-  {path: "produits", component : ProductList}
+  { path: "products", component: ProductList },
+  { path: "product-create", component: ProductCreate },
+  { path: "", redirectTo: "products", pathMatch: "full" }
 ];
