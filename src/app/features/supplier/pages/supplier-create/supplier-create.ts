3a12461ffda016e3f97242c58f7258252e07fb71
Ayoub Gaouet <gaouetayoub1@gmail.com>
2026-03-02 22:07:22 +0100

    add supplier management: create supplier list, create and edit components

---
+++ src/app/features/supplier/pages/supplier-create/supplier-create.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier-create',
+  imports: [],
+  templateUrl: './supplier-create.html',
+  styleUrl: './supplier-create.css',
+})
+export class SupplierCreate {
+
+}
