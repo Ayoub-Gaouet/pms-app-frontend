a850650ca3f7da22d3149a2308eb021fb0f7a389
Ayoub Gaouet <gaouetayoub1@gmail.com>
2026-03-07 20:24:09 +0100

    Add TechnicianModel and SkillModel classes
    Introduce TechnicianModel and SkillModel to represent technicians and their skills. These models include properties like IDs, names, and timestamps for creation and updates. They lay the groundwork for managing technician-related data in the application.

---
+++ src/app/features/technician/models/skill.model.ts
@@ -0,0 +1,6 @@
+export class SkillModel {
+  id?: number;
+  name?: string;
+  created_at!: Date;
+  updated_at!: Date;
+}
---
+++ src/app/features/technician/models/technician.model.ts
@@ -0,0 +1,10 @@
+import {SkillModel} from './skill.model';
+
+export class TechnicianModel{
+  id?: number;
+  nom?: string;
+  skill?: SkillModel;
+  created_at?: Date;
+  updated_at?: Date;
+
+}
